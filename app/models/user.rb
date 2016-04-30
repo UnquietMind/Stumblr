899afeef85cdeb463b9b4865e68f99451f8745d7
unquietmind <unquietmind9@gmail.com>
2016-04-30 15:47:27 +0000

    Installed tiddle gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ActiveRecord::Base
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         :token_authenticatable
+         
+  has_many :authentication_tokens
 end
